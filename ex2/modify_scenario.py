deae30a05c2ce3bc7781841f51ea79ea3b0206d9
Alexander Yaroshevich <alexander.yaroshevich@apalon.com>
2023-05-10 15:59:39 +0200

    add the doc and refactor

--- ex2/modify_scenario.py
+++ ex2/modify_scenario.py
@@ -1,16 +1,15 @@
+import argparse
 import json
 from pathlib import Path
 
 
-def main():
-    scenario_path = Path(
-        '/home/a_yaroshevich/projects/tum/vadere/Scenarios/Demos/bus_station/scenarios/bus_station.scenario')
-    save_path = scenario_path.with_name('modified.scenario')
-    with open(scenario_path) as f:
-        scenario = json.load(f)
+def create_additional_pedestrian():
+    """Creates a dictionary representing an additional pedestrian to be added to the scenario.
 
-    scenario["name"] = scenario["name"] + "_modified"
-    additional_pedestrian = {
+    Returns:
+        A dictionary with keys representing the attributes of the pedestrian.
+    """
+    return {
         "id": 7,
         "shape": {
             "x": 11.0,
@@ -38,10 +37,35 @@ def main():
         },
         "groupSizeDistribution": [1.0]
     }
+
+
+def main(args):
+    """Modifies a Vadere scenario by adding an additional pedestrian.
+
+    Args:
+        args: An argparse.Namespace object containing parsed command-line arguments.
+
+    Returns:
+        None.
+    """
+    scenario_path = Path(args.scenario)
+    save_path = Path(args.output) if args.output else scenario_path.with_name('modified.scenario')
+
+    with scenario_path.open() as f:
+        scenario = json.load(f)
+
+    scenario["name"] = f"{scenario['name']}_modified"
+    additional_pedestrian = create_additional_pedestrian()
     scenario['scenario']['topography']['sources'].append(additional_pedestrian)
-    with open(save_path, 'w') as f:
+
+    with save_path.open('w') as f:
         json.dump(scenario, f, indent=2)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scenario', help='path to the scenario JSON file',
+                        default='/home/a_yaroshevich/projects/tum/vadere/Scenarios/Demos/bus_station/scenarios/bus_station.scenario')
+    parser.add_argument('--output', help='path to the output file (optional)')
+    args = parser.parse_args()
+    main(args)
