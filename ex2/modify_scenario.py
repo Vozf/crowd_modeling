ab8bcb82b29b60d9c2a796d7e24c9b7f97ff528f
Alexander Yaroshevich <alexander.yaroshevich@apalon.com>
2023-05-10 16:36:09 +0200

    use dynamci elements

--- ex2/modify_scenario.py
+++ ex2/modify_scenario.py
@@ -10,32 +10,70 @@ def create_additional_pedestrian():
         A dictionary with keys representing the attributes of the pedestrian.
     """
     return {
-        "id": 7,
-        "shape": {
+        "attributes": {
+            "id": 1,
+            "shape": {
+                "x": 0.0,
+                "y": 0.0,
+                "width": 1.0,
+                "height": 1.0,
+                "type": "RECTANGLE"
+            },
+            "visible": True,
+            "radius": 0.2,
+            "densityDependentSpeed": False,
+            "speedDistributionMean": 1.34,
+            "speedDistributionStandardDeviation": 0.26,
+            "minimumSpeed": 0.5,
+            "maximumSpeed": 2.2,
+            "acceleration": 2.0,
+            "footstepHistorySize": 4,
+            "searchRadius": 1.0,
+            "walkingDirectionSameIfAngleLessOrEqual": 45.0,
+            "walkingDirectionCalculation": "BY_TARGET_CENTER"
+        },
+        "source": None,
+        "targetIds": [2],
+        "nextTargetListIndex": 0,
+        "isCurrentTargetAnAgent": False,
+        "position": {
             "x": 11.0,
-            "y": 17.0,
-            "width": 1.0,
-            "height": 1.0,
-            "type": "RECTANGLE"
+            "y": 1.0
         },
-        "visible": True,
-        "targetIds": [2, 1],
-        "spawner": {
-            "type": "org.vadere.state.attributes.spawner.AttributesRegularSpawner",
-            "constraintsElementsMax": -1,
-            "constraintsTimeStart": 0.0,
-            "constraintsTimeEnd": 0.0,
-            "eventPositionRandom": False,
-            "eventPositionGridCA": False,
-            "eventPositionFreeSpace": True,
-            "eventElementCount": 1,
-            "eventElement": None,
-            "distribution": {
-                "type": "org.vadere.state.attributes.distributions.AttributesConstantDistribution",
-                "updateFrequency": 1.0
-            }
+        "velocity": {
+            "x": 0.0,
+            "y": 0.0
         },
-        "groupSizeDistribution": [1.0]
+        "freeFlowSpeed": 1.4648429039159518,
+        "followers": [],
+        "idAsTarget": -1,
+        "isChild": False,
+        "isLikelyInjured": False,
+        "psychologyStatus": {
+            "mostImportantStimulus": None,
+            "threatMemory": {
+                "allThreats": [],
+                "latestThreatUnhandled": False
+            },
+            "selfCategory": "TARGET_ORIENTED",
+            "groupMembership": "OUT_GROUP",
+            "knowledgeBase": {
+                "knowledge": [],
+                "informationState": "NO_INFORMATION"
+            },
+            "perceivedStimuli": [],
+            "nextPerceivedStimuli": []
+        },
+        "healthStatus": None,
+        "infectionStatus": None,
+        "groupIds": [],
+        "groupSizes": [],
+        "agentsInGroup": [],
+        "trajectory": {
+            "footSteps": []
+        },
+        "modelPedestrianMap": {},
+        "type": "PEDESTRIAN"
     }
 
 
@@ -56,7 +94,7 @@ def main(args):
 
     scenario["name"] = f"{scenario['name']}_modified"
     additional_pedestrian = create_additional_pedestrian()
-    scenario['scenario']['topography']['sources'].append(additional_pedestrian)
+    scenario['scenario']['topography']['dynamicElements'].append(additional_pedestrian)
 
     with save_path.open('w') as f:
         json.dump(scenario, f, indent=2)
@@ -65,7 +103,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--scenario', help='path to the scenario JSON file',
-                        default='/home/a_yaroshevich/projects/tum/vadere/Scenarios/Demos/bus_station/scenarios/bus_station.scenario')
+                        default='/home/a_yaroshevich/projects/tum/vadere/Scenarios/Demos/bus_station/scenarios/scenario_2.scenario')
     parser.add_argument('--output', help='path to the output file (optional)')
     args = parser.parse_args()
     main(args)
